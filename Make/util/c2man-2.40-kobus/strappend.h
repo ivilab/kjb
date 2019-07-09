7a4a95f2a95491de56e809641dc6fb3011d04b25
ivilab-sync <52473452+ivilab-sync@users.noreply.github.com>
2019-07-08 19:25:01 -0700

    Kill!

--- Make/util/c2man-2.40-kobus/strappend.h
+++
@@ -1,6 +0,0 @@
-/* $Id: strappend.h,v 2.0.1.1 1993/05/31 01:38:48 greyham Exp $
- * concatenate a list of strings, storing them in a malloc'ed region
- */
-#include "config.h"
-
-char *strappend _V((char *first, ...));
